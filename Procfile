61742477aa241cd7f451a5dbc9a33eb94fbcdb86
Rayappa Raju <rrr250684@gmail.com>
2026-06-01 19:00:58 +0530

    Fix Render deployment: auto-create admin on startup, Procfile update for demo data

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn school.wsgi --log-file -
-release: python manage.py migrate --noinput
+release: python manage.py migrate --noinput && python manage.py add_demo_data --noinput
--- schoolapp/apps.py
+++ schoolapp/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def create_default_admin(sender, **kwargs):
+    from django.contrib.auth.models import User
+    from schoolapp.models import Notice
+    if not User.objects.filter(is_superuser=True).exists():
+        User.objects.create_superuser('admin', 'admin@school.com', 'admin123')
+    if Notice.objects.count() == 0:
+        Notice.objects.create(
+            title='Welcome to SchoolMS',
+            content='School Management System is up and running. Use admin/admin123 to login.',
+        )
 
 
 class SchoolappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'schoolapp'
+
+    def ready(self):
+        post_migrate.connect(create_default_admin, sender=self)
